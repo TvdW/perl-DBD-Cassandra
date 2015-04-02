8b25d99d150127acf37cad7655641b8973b16eee
Tom van der Woerdt <info@tvdw.eu>
2015-04-02 19:45:45 +0200

    Better UTF-8 support, now also in the decoder

--- DBD-Cassandra/lib/DBD/Cassandra/Type.pm
+++ DBD-Cassandra/lib/DBD/Cassandra/Type.pm
@@ -6,23 +6,23 @@ require Exporter;
 our @ISA= 'Exporter';
 
 my %lookup= (
-    0  => [\&p2c_string, \&c2p_string, 'TYPE_CUSTOM'],
-    1  => [\&p2c_string, \&c2p_string, 'TYPE_ASCII'],
-    2  => [\&p2c_bigint, \&c2p_bigint, 'TYPE_BIGINT'],
-    3  => [\&p2c_string, \&c2p_string, 'TYPE_BLOB'],
-    4  => [\&p2c_bool,   \&c2p_bool,   'TYPE_BOOLEAN'],
-    5  => [\&not_impl,   \&not_impl,   'TYPE_COUNTER'],
-    6  => [\&not_impl,   \&not_impl,   'TYPE_DECIMAL'],
-    7  => [\&p2c_double, \&c2p_double, 'TYPE_DOUBLE'],
-    8  => [\&p2c_float,  \&c2p_float,  'TYPE_FLOAT'],
-    9  => [\&p2c_int,    \&c2p_int,    'TYPE_INT'],
-    10 => [\&p2c_string, \&c2p_string, 'TYPE_TEXT'],
-    11 => [\&not_impl,   \&not_impl,   'TYPE_TIMESTAMP'],
-    12 => [\&not_impl,   \&not_impl,   'TYPE_UUID'],
-    13 => [\&p2c_string, \&c2p_string, 'TYPE_VARCHAR'],
-    14 => [\&not_impl,   \&not_impl,   'TYPE_VARINT'],
-    15 => [\&not_impl,   \&not_impl,   'TYPE_TIMEUUID'],
-    16 => [\&not_impl,   \&not_impl,   'TYPE_INET'],
+    0  => [\&p2c_string, \&c2p_string,      'TYPE_CUSTOM'],
+    1  => [\&p2c_string, \&c2p_string,      'TYPE_ASCII'],
+    2  => [\&p2c_bigint, \&c2p_bigint,      'TYPE_BIGINT'],
+    3  => [\&p2c_string, \&c2p_string,      'TYPE_BLOB'],
+    4  => [\&p2c_bool,   \&c2p_bool,        'TYPE_BOOLEAN'],
+    5  => [\&not_impl,   \&not_impl,        'TYPE_COUNTER'],
+    6  => [\&not_impl,   \&not_impl,        'TYPE_DECIMAL'],
+    7  => [\&p2c_double, \&c2p_double,      'TYPE_DOUBLE'],
+    8  => [\&p2c_float,  \&c2p_float,       'TYPE_FLOAT'],
+    9  => [\&p2c_int,    \&c2p_int,         'TYPE_INT'],
+    10 => [\&p2c_string, \&c2p_utf8string,  'TYPE_TEXT'],
+    11 => [\&not_impl,   \&not_impl,        'TYPE_TIMESTAMP'],
+    12 => [\&not_impl,   \&not_impl,        'TYPE_UUID'],
+    13 => [\&p2c_string, \&c2p_utf8string,  'TYPE_VARCHAR'],
+    14 => [\&not_impl,   \&not_impl,        'TYPE_VARINT'],
+    15 => [\&not_impl,   \&not_impl,        'TYPE_TIMEUUID'],
+    16 => [\&not_impl,   \&not_impl,        'TYPE_INET'],
 );
 
 sub not_impl { ... }
@@ -40,6 +40,7 @@ sub p2c_string {
     return ("pack('l>/a', \$_[$i])", "utf8::encode \$_[$i]");
 }
 sub c2p_string { return shift }
+sub c2p_utf8string { my $var= shift; return ($var, "utf8::decode $var") }
 sub p2c_bigint { return   _pack('q>', 8, @_) }
 sub c2p_bigint { return _unpack('q>', 8, @_) }
 sub p2c_int { return   _pack('l>', 4, @_) }
@@ -89,10 +90,13 @@ sub build_row_decoder {
     for my $type (@$types) {
         if (ref $type) { $type= $type->{type}; }
         my $t= $lookup{$type} or die "Unknown type $type";
+        my ($c, $prep)= $t->[1]('$b');
+
         $code .= '    $a= unpack("l>", substr $_[0], 0, 4, "");'."\n";
         $code .= '    if ($a >= 0) {'."\n";
         $code .= '        $b= substr $_[0], 0, $a, "";'."\n";
-        $code .= '        push @row, ('.$t->[1]('$b').');'."\n";
+        $code .= '        '.$prep.';'."\n" if $prep;
+        $code .= '        push @row, ('.$c.');'."\n";
         $code .= '    } else { push @row, undef; }'."\n";
         $i++;
     }
---
+++ DBD-Cassandra/t/20-utf8.t
@@ -0,0 +1,26 @@
+use v5.14;
+use DBI;
+use Test::More;
+
+unless ($ENV{CASSANDRA_HOST}) {
+    plan skip_all => "CASSANDRA_HOST not set";
+}
+
+plan tests => 2;
+
+my $dbh= DBI->connect("dbi:Cassandra:host=$ENV{CASSANDRA_HOST};keyspace=dbd_cassandra_tests", undef, undef, {RaiseError => 1});
+ok($dbh);
+
+$dbh->do('create table if not exists test_utf8 (id bigint primary key, str varchar)');
+
+my $test_string= "";
+utf8::decode($test_string);
+for (1..100) {
+    $test_string .= chr(int(rand(30000)+1000));
+}
+
+$dbh->do("insert into test_utf8 (id, str) values (?,?)", undef, 1, $test_string);
+my $row= $dbh->selectrow_arrayref('select str from test_utf8 where id=1');
+ok($row->[0] eq $test_string);
+
+$dbh->disconnect;
