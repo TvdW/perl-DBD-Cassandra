8bf23cf76446bde138b02e5b6bdfce536f4997c3
Tom van der Woerdt <info@tvdw.eu>
2015-04-17 12:25:56 +0200

    Cache prepared statements for 60 seconds. Mostly because of DBI's do()

--- DBD-Cassandra/lib/DBD/Cassandra/db.pm
+++ DBD-Cassandra/lib/DBD/Cassandra/db.pm
@@ -11,44 +11,56 @@ $DBD::Cassandra::db::imp_data_size = 0;
 sub prepare {
     my ($dbh, $statement, $attribs)= @_;
 
-    my ($opcode, $body);
-    eval {
-        ($opcode, $body)= $dbh->{cass_connection}->request(
-            OPCODE_PREPARE,
-            pack_longstring($statement)
-        );
-        1;
-    } or do {
-        my $error= $@ || "unknown error";
-        return $dbh->set_err($DBI::stderr, "prepare failed: $error");
-    };
+    my $prepared= $dbh->{cass_prepare_cache}{$statement};
+    undef $prepared if $prepared && $prepared->[0] < time() - 60;
+    $prepared //= (do {
+        my ($opcode, $body);
+        eval {
+            ($opcode, $body)= $dbh->{cass_connection}->request(
+                OPCODE_PREPARE,
+                pack_longstring($statement)
+            );
+            1;
+        } or do {
+            my $error= $@ || "unknown error";
+            return $dbh->set_err($DBI::stderr, "prepare failed: $error");
+        };
 
-    if ($opcode != OPCODE_RESULT) {
-        return $dbh->set_err($DBI::stderr, "Unknown response from server");
-    }
+        if ($opcode != OPCODE_RESULT) {
+            return $dbh->set_err($DBI::stderr, "Unknown response from server");
+        }
 
-    my $kind= unpack('N', substr $body, 0, 4, '');
-    if ($kind != RESULT_PREPARED) {
-        return $dbh->set_err($DBI::stderr, "Server returned an unknown response");
-    }
+        my $kind= unpack('N', substr $body, 0, 4, '');
+        if ($kind != RESULT_PREPARED) {
+            return $dbh->set_err($DBI::stderr, "Server returned an unknown response");
+        }
 
-    my $prepared_id= unpack_shortbytes($body);
-    my $metadata= unpack_metadata($body);
-    my $result_metadata= unpack_metadata($body);
-    my $paramcount= 0+ @{ $metadata->{columns} };
-    my @names= map { $_->{name} } @{$result_metadata->{columns}};
+        my $prepared_id= unpack_shortbytes($body);
+        my $metadata= unpack_metadata($body);
+        my $result_metadata= unpack_metadata($body);
+        my $paramcount= 0+ @{ $metadata->{columns} };
+        my @names= map { $_->{name} } @{$result_metadata->{columns}};
+
+        [undef, {
+            cass_prepared_metadata => $metadata,
+            cass_prepared_result_metadata => $result_metadata,
+            cass_prepared_id => $prepared_id,
+            cass_row_encoder => build_row_encoder($metadata->{columns}),
+            cass_row_decoder => build_row_decoder($result_metadata->{columns}),
+            NAME => \@names,
+            NUM_OF_FIELDS => 0+@names,
+            NUM_OF_PARAMS => $paramcount,
+        }]
+    });
+    $prepared->[0]= time();
 
     my ($outer, $sth)= DBI::_new_sth($dbh, { Statement => $statement });
-    $sth->STORE('NUM_OF_PARAMS', $paramcount);
-    $sth->STORE('NUM_OF_FIELDS', 0+@names);
-    $sth->{NAME}= \@names;
-
+    for my $key (keys %{$prepared->[1]}) {
+        $key =~ /^NUM_/
+            ? $sth->STORE($key, $prepared->[1]{$key})
+            : ($sth->{$key}= $prepared->[1]{$key});
+    }
     $sth->{cass_params}= [];
-    $sth->{cass_prepared_metadata}= $metadata;
-    $sth->{cass_prepared_result_metadata}= $result_metadata;
-    $sth->{cass_prepared_id}= $prepared_id;
-    $sth->{cass_row_encoder}= build_row_encoder($metadata->{columns});
-    $sth->{cass_row_decoder}= build_row_decoder($result_metadata->{columns});
     $sth->{cass_consistency}= $attribs->{consistency} // $attribs->{Consistency} // $dbh->{cass_consistency} // 'one';
     return $outer;
 }
