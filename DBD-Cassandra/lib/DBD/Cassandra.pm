677911e3e583cd1d461110923d4aecd9ad507bea
Tom van der Woerdt <info@tvdw.eu>
2015-10-08 16:07:04 +0200

    0.22

--- DBD-Cassandra/lib/DBD/Cassandra.pm
+++ DBD-Cassandra/lib/DBD/Cassandra.pm
@@ -6,7 +6,7 @@ use DBD::Cassandra::dr;
 use DBD::Cassandra::db;
 use DBD::Cassandra::st;
 
-our $VERSION= '0.21';
+our $VERSION= '0.22';
 our $drh= undef;
 
 sub driver {
@@ -273,6 +273,10 @@ supported:
 =item * float
 
 =item * int
+
+=item * list
+
+=item * set
 
 =item * text
 
